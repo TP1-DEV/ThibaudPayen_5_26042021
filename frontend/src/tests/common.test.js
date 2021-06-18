69840dcb5aabffde172e1e679826ee862e73abdb
Thibaud PAYEN <thg.payen@gmail.com>
2021-06-18 12:29:03 +0200

    Jest

---
+++ frontend/src/tests/common.test.js
@@ -0,0 +1,14 @@
+import { getSearchParams } from "../js/common"
+
+/* describe('get data', () => {
+it('should return data when function is called', () => {
+    const data = commonFn.getData()
+    expect(data).toReturn(data)
+})
+}) */
+
+describe('getUrlSearchParams', () => {
+it('should return the value of search in url', () => {
+   
+})
+})
